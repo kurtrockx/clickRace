9fc28d56c338908e655b8c1e53ce59c609278ff7
derpfear <kurtdebelen431@gmail.com>
2025-02-05 23:57:09 +0800

    functional timer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [timer, setTimer] = useState(10);
+
+  const handleSetTimer = (timer) => {
+    setTimer(timer);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Timer timer={timer} onSetTimer={handleSetTimer} />
     </div>
+  );
+}
+
+function Timer({ timer, onSetTimer }) {
+  const handleIncreaseTimer = () => {
+    if (timer >= 20) return;
+    onSetTimer((t) => t + 1);
+  };
+  const handleDecreaseTimer = () => {
+    if (timer <= 5) return;
+    onSetTimer((t) => t - 1);
+  };
+
+  const handleStartTimer = () => {
+    const intervalId = setInterval(() => {
+      onSetTimer((t) => {
+        if (t <= 1) {
+          clearInterval(intervalId);
+          return 0;
+        }
+        return t - 1;
+      });
+    }, 1000);
+  };
+
+  return (
+    <div className="timer-wrapper">
+      <TimerButton onClick={handleDecreaseTimer}>◀</TimerButton>
+      <h1 className="timer">{timer}</h1>
+      <TimerButton onClick={handleIncreaseTimer}>▶</TimerButton>
+
+      <button onClick={() => handleStartTimer()}>Start Game</button>
+    </div>
+  );
+}
+
+function TimerButton({ children, onClick }) {
+  return (
+    <button className="timer-button" onClick={onClick}>
+      {children}
+    </button>
   );
 }
 
